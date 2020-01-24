9af5bc09d75d4f50d7118ed83d48ce5aa1a51c55
Shaolin P <shiulan.poe@outlook.com>
2020-01-24 11:58:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_L1_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
